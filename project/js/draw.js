20fed3c799aa8f23365ef1a050b6f0eb8a3a491f
huy8895 <huy8895@gmail.com>
2020-06-26 14:18:21 +0700

    sua them tinh nang di chuyen cho snake - cai dat thong so scale: la toc do di chuyen thong so size: kich thuoc cua ran

--- project/js/draw.js
+++ project/js/draw.js
@@ -1,24 +1,32 @@
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
-let scale = 10;
+let scale = 5;
+let size = 30;
 let rows = canvas.height/scale;
 let cols = canvas.width/scale;
 
+let snake = new Snake();
+let fruit = new Fruit();
+let stop;
 
-(function setup() {
-    let snake = new Snake();
-    let fruit = new Fruit();
+/*(function setup() {*/
     function loop() {
         ctx.clearRect(0,0,canvas.width,canvas.height)
         snake.draw();
         snake.update();
-        requestAnimationFrame(loop)
+        stop = requestAnimationFrame(loop)
+        console.log(snake.x);
+        console.log(snake.y);
+        console.log(canvas.width);
+        console.log(canvas.height);
 
     }
-    requestAnimationFrame(loop)
-})()
 
+/*})()*/
+cancelAnimationFrame(stop);
 
 window.addEventListener('keydown', ((evt) => {
     const direction = evt.key.replace('Arrow', '');
+    console.log(direction);
+    snake.changeDirection(direction);
 }));
--- project/js/snake.js
+++ project/js/snake.js
@@ -1,7 +1,7 @@
 let Snake = function () {
     this.x = 0;
     this.y = 0;
-    this.xSpeed = scale*1;
+    this.xSpeed = scale;
     this.ySpeed = 0;
     this.total = 0;
     this.tail = [];
@@ -10,10 +10,10 @@ let Snake = function () {
         ctx.fillStyle = 'blue';
         if(this.tail.length > 0){
             for (let i = 0; this.tail.length; i ++){
-                ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale)
+                ctx.fillRect(this.tail[i].x, this.tail[i].y, size, size)
             }
         }
-        ctx.fillRect(this.x,this.y,scale,scale);
+        ctx.fillRect(this.x,this.y,size,size);
     }
     this.update = function () {
         for (let i = 0; i < this.tail.length - 1; i ++){
@@ -27,10 +27,10 @@ let Snake = function () {
         this.x += this.xSpeed;
         this.y += this.ySpeed;
 
-        if (this.x > canvas.width){
+        if (this.x >= canvas.width){
             this.x = 0
         }
-        if (this.y > canvas.height){
+        if (this.y >= canvas.height){
             this.y = 0
         }
         if (this.x < 0) {
@@ -38,6 +38,27 @@ let Snake = function () {
         }
         if (this.y < 0) {
             this.y = canvas.height - scale;
+        }
+    }
+
+    this.changeDirection = function(direction){
+        switch(direction){
+            case 'Up':
+                this.xSpeed = 0;
+                this.ySpeed = -scale;
+                break;
+            case 'Down':
+                this.xSpeed = 0;
+                this.ySpeed = scale;
+                break;
+            case 'Left':
+                this.xSpeed = -scale;
+                this.ySpeed = 0;
+                break;
+            case 'Right':
+                this.xSpeed = +scale;
+                this.ySpeed = 0;
+                break;
         }
     }
 
